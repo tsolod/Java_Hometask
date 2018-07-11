1c7ea34aa2203c72b6912a864d9a36179aa93d0d
tsolod <40228890+tsolod@users.noreply.github.com>
2018-07-11 22:25:41 +0300

    Задание 3 (+комменты)

--- sandbox/src/test/java/ru/stqa/javahome/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/javahome/sandbox/PointTest.java
@@ -5,19 +5,19 @@ import org.testng.annotations.Test;
 
 public class PointTest {
   @Test
-  public void testDistance1(){
+  public void testDistance1(){ //от начала координат до точки
     Point p1 = new Point(0,0);
     Point p2 = new Point (7,2);
     Assert.assertEquals( Math.round(p1.distance(p2)), 7 );
   }
   @Test
-  public void testDistance2(){
+  public void testDistance2(){ //от самой себя
   Point p1 = new Point (7,2);
   Assert.assertEquals( Math.round(p1.distance(p1)), 0 );
 }
 
   @Test
-  public void testDistance3(){
+  public void testDistance3(){ //из противоположных областей
     Point p1 = new Point (-7,2);
     Point p2 = new Point (7,-2);
     Assert.assertEquals( Math.round(p1.distance(p2)), 15 );
