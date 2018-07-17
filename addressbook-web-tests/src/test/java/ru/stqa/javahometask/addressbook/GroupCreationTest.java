d14ce140656a528c8f303b0024916f0b46ffb49e
tsolod <40228890+tsolod@users.noreply.github.com>
2018-07-17 22:23:18 +0300

    Добавлен класс GroupData

--- addressbook-web-tests/src/test/java/ru/stqa/javahometask/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javahometask/addressbook/GroupCreationTest.java
@@ -4,16 +4,11 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class GroupCreationTest {
     FirefoxDriver wd;
@@ -23,32 +18,60 @@ public class GroupCreationTest {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+
     }
-    
+
+
     @Test
     public void testGroupCreation() {
-        wd.findElement(By.linkText("groups")).click();
-        wd.findElement(By.name("new")).click();
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("test2");
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("ret");
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("ter");
-        wd.findElement(By.name("submit")).click();
+        getGroupsPage();
+        doNewGroup();
+        putInfoGroup(new GroupData("test1", "test2", "test3"));
+        saveNewGrop();
+        returnToGroupsPage();
+    }
+
+    private void returnToGroupsPage() {
         wd.findElement(By.linkText("group page")).click();
     }
-    
+
+    private void saveNewGrop() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void putInfoGroup(GroupData groupData) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHead());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFeet());
+
+
+    }
+
+    private void doNewGroup() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    private void getGroupsPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
---
+++ addressbook-web-tests/src/test/java/ru/stqa/javahometask/addressbook/GroupData.java
@@ -0,0 +1,25 @@
+package ru.stqa.javahometask.addressbook;
+
+public class GroupData {
+  private final String name;
+  private final String head;
+  private final String feet;
+
+  public GroupData(String name, String head, String feet) {
+    this.name = name;
+    this.head = head;
+    this.feet = feet;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getHead() {
+    return head;
+  }
+
+  public String getFeet() {
+    return feet;
+  }
+}
