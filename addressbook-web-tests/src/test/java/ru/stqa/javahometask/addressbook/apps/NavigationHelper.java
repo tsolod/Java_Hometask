228bc442217fc7161bf251562953a1e853fb5a7a
tsolod <40228890+tsolod@users.noreply.github.com>
2018-08-19 00:59:16 +0300

    оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/javahometask/addressbook/apps/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javahometask/addressbook/apps/NavigationHelper.java
@@ -2,7 +2,6 @@ package ru.stqa.javahometask.addressbook.apps;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigationHelper extends HelperBase{
 
@@ -11,14 +10,26 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void getGroupsPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Group")
+            && isElementPresent(By.name("new")) ) {
+      return;
+    }
       click(By.linkText("groups"));
   }
 
   public void getAddNewContact() {
+    if((isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry"))) {
+      return;
+    }
     click(By.linkText("add new"));
   }
 
   public void getHome() {
+    if(isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home"));
   }
 }
